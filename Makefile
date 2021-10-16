23aeedb52c038038cdb4bfc42284d13804e1eaac
Arne Neumann <github@arne.cl>
2021-10-16 23:20:57 +0200

    fix UDPIPE setup and tests

--- Makefile
+++ Makefile
@@ -1,15 +1,19 @@
 docker-build:
 	docker build -t udpipe1 -f Dockerfile-udpipe-1 .
 
-docker-run:
+docker-run-en:
 	docker run -v ~/repos/braud-2017-service/udpipe-models:/models \
 	    -p 9001:9001 \
-	    -e MODEL_FILE_NAME=english-ewt-ud-2.5-191206.udpipe \
-	    -e MODEL_NAME=english-ewt \
+	    -e DEFAULT_MODEL=english-ewt \
+	    -it udpipe1
+
+docker-run-de:
+	docker run -v ~/repos/braud-2017-service/udpipe-models:/models \
+	    -p 9001:9001 \
+	    -e DEFAULT_MODEL=german-hdt \
 	    -it udpipe1
 
 test:
-	#curl "http://localhost:9001/process?tokenizer&tagger&parser&data=He%20died%20yesterday." | PYTHONIOENCODING=utf-8 python -c "import sys,json; sys.stdout.write(json.load(sys.stdin)['result'])"
 	curl -X POST -F "data=@tests/fixtures/input_eurostar_en.txt" -F "tokenizer=" -F "tagger=" -F "parser=" "http://localhost:9001/process" | PYTHONIOENCODING=utf-8 python -c "import sys,json; sys.stdout.write(json.load(sys.stdin)['result'])"
 
 docker-test:
--- tests/test_udpipe.py
+++ tests/test_udpipe.py
@@ -10,17 +10,43 @@ import requests
 
 
 UDPIPE1_ENDPOINT = os.environ.get('UDPIPE1_ENDPOINT', 'http://localhost:9090')
+ENGLISH_MODEL = 'english-ewt-ud-2.5-191206.udpipe'
+GERMAN_MODEL = 'german-hdt-ud-2.5-191206.udpipe'
 
 
-def test_uppipe_api():
+def test_available_models():
+    """UDPIPE API serves the expected English and German models."""
+    res = requests.get(f'{UDPIPE1_ENDPOINT}/models')
+    res_dict = res.json()
+    assert ENGLISH_MODEL in res_dict['models']
+    assert GERMAN_MODEL in res_dict['models']
+
+    # UDPIPE defaults to English
+    assert ENGLISH_MODEL == res_dict['default_model']
+
+
+def test_uppipe_api_english():
     """A plaintext file can be parsed with UDPIPE 1 via its REST API."""
     input_text = Path('tests/fixtures/input_eurostar_en.txt').read_text()
 
     res = requests.post(
         '{}/process'.format(UDPIPE1_ENDPOINT),
-        data={'tokenizer': '', 'tagger': '', 'parser': ''},
+        data={'model': ENGLISH_MODEL, 'tokenizer': '', 'tagger': '', 'parser': ''},
         files={'data': input_text})
 
     udpipe_output = res.json()['result']
     expected_udpipe_output = Path('tests/fixtures/input_eurostar_en.txt.conllu').read_text()
+    assert udpipe_output == expected_udpipe_output
+
+def test_uppipe_api_german():
+    """A plaintext file can be parsed with UDPIPE 1 via its REST API."""
+    input_text = Path('tests/fixtures/input_eurostar_de.txt').read_text()
+
+    res = requests.post(
+        '{}/process'.format(UDPIPE1_ENDPOINT),
+        data={'model': GERMAN_MODEL, 'tokenizer': '', 'tagger': '', 'parser': ''},
+        files={'data': input_text})
+
+    udpipe_output = res.json()['result']
+    expected_udpipe_output = Path('tests/fixtures/input_eurostar_de.txt.conllu').read_text()
     assert udpipe_output == expected_udpipe_output
